ad1f7d3541b209887748ea014c9e4ef2760aed08
Tatiana Villa Ema <tatiana@tecnologia-facil.es>
2025-12-20 23:22:11 +0100

    Mejoras significativas en todas las páginas

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -8,7 +8,6 @@ namespace Portafolio.Controllers
     {
         public IActionResult Index()
         {
-            ViewBag.Edad = 99;
             return View();
         }
 
@@ -17,10 +16,59 @@ namespace Portafolio.Controllers
             return View();
         }
 
+        public IActionResult Formacion()
+        {
+            return View();
+        }
+
+        public IActionResult Experiencia()
+        {
+            return View();
+        }
+
+        public IActionResult Contacto()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
         }
+
+        private readonly IEmailSender _emailSender;
+
+        public HomeController(IEmailSender emailSender)
+        {
+            _emailSender = emailSender;
+        }
+
+        [HttpGet]
+        public IActionResult Hablamos()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Contacto (string Nombre, string Email, string Mensaje)
+        {
+            string contenido =
+                    $@"Nuevo mensaje desde el portfolio:
+
+                    Nombre: {Nombre}
+                    Email: {Email}
+
+                    Mensaje:
+                    {Mensaje}
+                    ";
+
+            await _emailSender.SendEmailAsync("tatiana@villaema.es", "Nuevo mensaje desde tu portfolio", contenido);
+
+            ViewBag.Enviado = true;
+            return View();
+        }
+
+        /* Ya estaba */
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
--- Program.cs
+++ Program.cs
@@ -1,7 +1,9 @@
-var builder = WebApplication.CreateBuilder(args);
+﻿var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+builder.Services.Configure<EmailSettings>(builder.Configuration.GetSection("EmailSettings"));
+builder.Services.AddTransient<IEmailSender, EmailSender>();
 
 var app = builder.Build();
 
@@ -9,21 +11,19 @@ var app = builder.Build();
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
 app.UseHttpsRedirection();
+app.UseStaticFiles(); // ✅ ESTA es la forma correcta en MVC
+
 app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
+
